82b756be110a8fda296fef63868eecd70197c17f
Russell Whelan <russell.whelan@devopsgroup.com>
2022-02-08 15:29:10 +0000

    Add X-Ray

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-xray-sdk-go/xray"
 	"github.com/gorilla/handlers"
 )
 
 type Message struct {
 	Message    string
 	StatusCode int
+}
+
+func init() {
+	xray.Configure(xray.Config{
+		DaemonAddr:     "127.0.0.1:2000", // default
+		ServiceVersion: "1.2.3",
+	})
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
